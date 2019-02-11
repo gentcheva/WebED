ae141a0da180f7a7c357f1ba2dacfee6c4cda550
Unknown <117308883@umail.ucc.ie>
2019-02-11 22:50:13 +0000

    general fixes. lectures. download content
    pdfs for download. jquery for searching lectures by number.  css improvements

--- modules/test.js
+++ modules/test.js
@@ -1,19 +1,19 @@
 $(function(){
-    var id = 2;
+    var id = 1;
     
     var x = $("section").last().attr("id");
-    var y = x[1];
-
+    var y = parseInt(x[1]);
+    $(".lectureSearch").attr("placeholder","Lecture lookup by number: 1/"+y)
+    
     // hide all the lecture notes
     $(".lectures").hide(); 
     $("#l1").show(); // shows the first lecture
     // show next 
     
     $("#next").on("click" , function(){
-        $("#l" +(id-1)).fadeOut();
-        $("#l"+id).fadeIn();
-        $(".lectureNum").html("Lecture "+id);
-
+        $("#l" +(id)).fadeOut();
+        $("#l"+(id+1)).fadeIn();
+        $(".lectureNum").html("Lecture "+(id+1));
         if ( (id+1) > y){
             id = id
         }else{
@@ -23,16 +23,25 @@ $(function(){
     });
 
     // hide current show next
-    $("#prev").on("click" , function(){
-       
-        console.log(id);
-       
+    $("#prev").on("click" , function(){       
          if (id>=2){
             $("#l"+id).fadeOut();
             $("#l" +(id-1)).fadeIn();
             $(".lectureNum").html("Lecture "+(id-1));
             id -= 1;
         }
-       
     });
+
+    // searches for a lecture number
+    $(".lectureSearch").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        value = parseInt(value);
+        var lastLecture = parseInt(y);
+        if (value <= lastLecture){
+            $("#l" +(id)).fadeOut();
+            $("#l"+value).fadeIn();
+            $(".lectureNum").html("Lecture "+ value);
+            id = value
+            }
+        });
 });
