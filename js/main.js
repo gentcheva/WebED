d413322832b6b3224213959bded5ae9c22a0b1e1
Unknown <117308883@umail.ucc.ie>
2019-04-08 00:10:04 +0100

    Computer architecture summary

--- js/main.js
+++ js/main.js
@@ -1,45 +1,6 @@
-// $(function(){
-//     var id = 2;
-    
-//     var x = $("section").last().attr("id");
-//     var y = x[1];
-
-//     // hide all the lecture notes
-//     $(".lectures").hide(); 
-//     $("#l1").show(); // shows the first lecture
-//     // show next 
-    
-//     $("#next").on("click" , function(){
-//         $("#l" +(id-1)).fadeOut();
-//         $("#l"+id).fadeIn();
-//         $(".lectureNum").html("Lecture "+id);
-
-//         if ( (id+1) > y){
-//             id = id
-//         }else{
-//             id += 1;
-//         }
-        
-//     });
-
-//     // hide current show next
-//     $("#prev").on("click" , function(){
-       
-//         console.log(id);
-       
-//          if (id>=2){
-//             $("#l"+id).fadeOut();
-//             $("#l" +(id-1)).fadeIn();
-//             $(".lectureNum").html("Lecture "+(id-1));
-//             id -= 1;
-//         }
-       
-//     });
-// });
-
 $(function(){
+    try{
     var id = 1;
-    
     var x = $("section").last().attr("id");
     var y = parseInt(x[1]);
     $(".lectureSearch").attr("placeholder","Lecture lookup by number: 1/"+y)
@@ -84,5 +45,25 @@ $(function(){
             id = value
             }
         });
-
+    }
+    catch(err){
+        console.log("not on lecture slides page")
+    }
+    try{
+    
+        $(".overview h1").click(function () {
+                    $(this).next("section").toggle("slow")
+            });
+        
+        $(".overview img").click(function(){
+            if($(this).width() > 100){
+            $(this).css("width","100px")
+            } else{
+                $(this).css("width","auto")
+            }
+        });
+    }
+    catch(err){
+        console.log("not on overview page")
+    }
 });
